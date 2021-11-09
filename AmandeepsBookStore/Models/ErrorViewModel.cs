7866b582c79b320c9cbf4fa9ea28d5b485eca27c
AmandeepKaur0772966 <w0772966@myscc.ca>
2021-11-08 22:14:14 -0500

    Add project files.

---
+++ AmandeepsBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AmandeepsBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
