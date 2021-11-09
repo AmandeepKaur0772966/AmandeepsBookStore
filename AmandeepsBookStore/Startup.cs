6c459a3a334a7e9e000b4b952de8ff43faf43c9c
AmandeepKaur0772966 <w0772966@myscc.ca>
2021-11-09 02:52:12 -0500

    Modified startup file with using statement

--- AmandeepsBookStore/Startup.cs
+++ AmandeepsBookStore/Startup.cs
@@ -1,4 +1,5 @@
 using AmandeepsBookStore.Data;
+using AmandeepsBookStore.DataAccess.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
