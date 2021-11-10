b356f0c0763e86ff37d52be70c2e2cdc729760e4
AmandeepKaur0772966 <w0772966@myscc.ca>
2021-11-10 16:14:03 -0500

    Added repos for category and modified it

---
+++ AmandeepsBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using AmandeepsBooks.DataAccess.Repository.IRepository;
+using AmandeepsBooks.Models;
+using AmandeepsBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace AmandeepsBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
