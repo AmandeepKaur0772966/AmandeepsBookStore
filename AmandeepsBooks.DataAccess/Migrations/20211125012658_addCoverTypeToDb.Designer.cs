9e4cdb82c28fa8de8c92809aa1387d4f2d0aa51a
AmandeepKaur0772966 <w0772966@myscc.ca>
2021-11-24 20:31:32 -0500

    Added CoverType migrations

--- AmandeepsBooks.DataAccess/Migrations/20211125012658_addCoverTypeToDb.Designer.cs
+++ AmandeepsBooks.DataAccess/Migrations/20211125012658_addCoverTypeToDb.Designer.cs
@@ -4,14 +4,16 @@ using AmandeepsBookStore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AmandeepsBooks.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211125012658_addCoverTypeToDb")]
+    partial class addCoverTypeToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
