7507552a40b7baf8fadee46d92d5aa436a05e8cd
AmandeepKaur0772966 <w0772966@myscc.ca>
2021-11-24 20:17:13 -0500

    Added CoverTypeRepository.cs and ICoverTypeRepository and modified ApplicationDbContext.cs

---
+++ AmandeepsBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,33 @@
+﻿using AmandeepsBooks.DataAccess.Repository.IRepository;
+using AmandeepsBooks.Models;
+using AmandeepsBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace AmandeepsBooks.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public object Name { get; private set; }
+        public int Id { get; private set; }
+
+        public void Update(CoverTypeRepository coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = (string)coverType.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
